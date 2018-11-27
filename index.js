166271cd02d55bd0c49263b83c77ac922ee67654
Juan Ortega <juanakin0108@gmail.com>
2018-11-27 01:22:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ otherDeli  = ["Steven", "Blake", "Avi"];
 
   for( var i=0; i < otherDeli.length; i++ ) {
     
-   katzDeli.push("Welcome, " + otherDeli[i] + "." + " You are number 1 in line.");
+   katzDeli.push(otherDeli[i]);
     
   }
   
-  return katzDeli;
+  return "Welcome, " + otherDeli[i] + "." + " You are number 1 in line."
 }
