7d5f4bd4986516decb70c6dc5c034be9c8f87879
Juan Ortega <juanakin0108@gmail.com>
2018-11-28 03:49:03 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function takeANumber(katzDeli, name) {
+  
+  katzDeli.push(`${name}`);
+  
+  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+  
+}
+
+function nowServing(katzDeli) {
+  
+  if ( katzDeli === katzDeli.length ) {
+    
+     return `${name}`;
+  }
+  
+  else {
+   
+   return "There is nobody waiting to be served!";
+   
+  }
+}
+  
+  
